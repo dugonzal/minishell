4524a81a1a167b61e09d762f33915a44f63a99d8
dugonzal <ciclocontinuo2@gmail.com>
2023-03-11 00:06:07 +0000

    muestro la entrada mientras se espera un comando y agrego con read_history lo que retorna readline a line y implemento exit

--- src/minishell.c
+++ src/minishell.c
@@ -6,7 +6,7 @@
 /*   By: ciclo <ciclo@student.42.fr>                +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/03/10 10:01:34 by ciclo             #+#    #+#             */
-/*   Updated: 2023/03/10 10:05:56 by ciclo            ###   ########.fr       */
+/*   Updated: 2023/03/11 00:04:02 by ciclo            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,6 +14,20 @@
 
 int	main(int ac, char **av, char **env)
 {
-	printf ("hello world");
+	char *line;
+	int status;
+
+	(void)av;
+	(void)ac;
+	(void)env;
+	status = 1;
+	while (status)
+	{
+		line = readline (BLUE"minishell$> "RESET);
+		read_history (line);
+		if (ft_strncmp(line, "exit", 4) == 0)
+			status = 0;
+		printf ("[%s]\n", line);
+	}
 	return (0);
 }
