d2d83a226257673a92f1d9d85bf29f1579f6963d
sizquierdob <sizquierdo@gmail.com>
2023-05-15 17:14:41 +0200

    añaidendo &cmd

--- src/exec.c
+++ src/exec.c
@@ -6,7 +6,7 @@
 /*   By: sizquier <sizquier@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/04/13 15:48:30 by ciclo             #+#    #+#             */
-/*   Updated: 2023/05/15 16:32:23 by sizquier         ###   ########.fr       */
+/*   Updated: 2023/05/15 16:43:50 by sizquier         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -142,7 +142,7 @@ int builtins(t_cmd *cmd, t_data *data)
   }
  else if (!ft_strncmp(cmd->cmd[0], "unset", ft_strlen(cmd->cmd[0])))
   {
-    ft_unset_builtin(data->env); // no se muy bien donde apunta data. Unset recibe dos argumentos como parametro
+    ft_unset_builtin(&cmd->cmd[1],data->env); // no se muy bien donde apunta data. Unset recibe dos argumentos como parametro
     return (1);
   }
   return (0);
